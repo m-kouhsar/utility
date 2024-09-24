d40971f582117ac1ea258c9c14084c4a0f7ac3fc
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2024-09-24 16:08:41 +0100

    Update CpG.Enrichment.R

--- CpG.Enrichment.R
+++ CpG.Enrichment.R
@@ -1,38 +1,64 @@
-CpG.Enrichment <- function(listA , listB ,Background.Size = NA, Background.list = NA, use.annotation=F , arrayTypeA="EPIC", arrayTypeB = "EPIC"){
+CpG.Enrichment <- function(listA , listB ,listA.type = "illumina", listB.type="illumina", Background.Size = NA, 
+                           Background.list = NA, use.missMethyl=F , arrayTypeA="EPIC", arrayTypeB = "EPIC"){
   
-  # listA: A character vector of CpGs
-  # listB: A character vector of CpGs
+  #######################################################################################
+  # listA and listB: A character vector of CpG IDs (illumina) or Gene Symbol
+  # listA.type and listB.type: "illumina" if the corresponding list contains CpG IDs. "symbol" if corresponding list contains gene symbol
   # Background.list: A character vector of CpGs or NA. 
   # Background.Size: An intager value or NA. It will be ignored if use.annotation=T
+  # use.missMethyl: If TRUE the gsameth function in missMethyl package will be used. In this case both listA and listB must contain CpG IDs
+  
+  #######################################################################################
+  
   listA <- unique(listA[!is.na(listA)])
   listB <- unique(listB[!is.na(listB)])
   
-  if(use.annotation){
+  if(use.missMethyl){
     suppressMessages(library(missMethyl))
     arrayTypeA <- match.arg(arrayTypeA, choices = c("450K" , "EPIC"))
     arrayTypeB <- match.arg(arrayTypeB, choices = c("450K" , "EPIC"))
-    array2 <- missMethyl:::.getFlatAnnotation(array.type = arrayTypeB)
+    arrayB <- missMethyl:::.getFlatAnnotation(array.type = arrayTypeB)
     if(arrayTypeA != arrayTypeB){
+      warning("Two different array types!")
       array1 <- missMethyl:::.getFlatAnnotation(array.type = arrayTypeA)
       if(all(is.na(Background.list))){
-        all_cpg <- intersect(array1$cpg , array2$cpg)
+        all_cpg <- intersect(array1$cpg , arrayB$cpg)
       }else{
         all_cpg <- Background.list
       }
     }else{
       if(all(is.na(Background.list))){
-        all_cpg <- unique(array2$cpg)
+        all_cpg <- unique(arrayB$cpg)
       }else{
         all_cpg <- Background.list
       }
     }
     
-    listB = unique(array2$entrezid[array2$cpg %in% listB])
+    listB = unique(arrayB$entrezid[arrayB$cpg %in% listB])
     
     results <- suppressMessages(gsameth(sig.cpg = listA, collection = listB ,all.cpg = all_cpg, 
                        array.type = arrayTypeA, sig.genes=T))[,-4]
-    names(results) <- c("N.listB" , "N.Shared" , "P.value" , "Shared.Genes")
+    results <- cbind.data.frame(length(unique(listA)) , results)
+    names(results) <- c("N.listA","N.listB" , "N.Shared" , "P.value" , "Shared.Genes")
+    
     }else{
+      
+      listA.type = match.arg(listA.type , choices = c("illumina" , "symbol"))
+      listB.type = match.arg(listB.type , choices = c("illumina" , "symbol"))
+      
+      if(listA.type != listB.type){
+        if(listA.type == "symbol"){
+          message("Mapping listB to gene symbols...")
+          arrayB = missMethyl:::.getFlatAnnotation(array.type = arrayTypeB)
+          listB =  arrayB$symbol[arrayB$cpg %in% listB]
+        }
+        if(listB.type == "symbol"){
+          message("Mapping listA to gene symbols...")
+          arrayA = missMethyl:::.getFlatAnnotation(array.type = arrayTypeA)
+          listA =  arrayA$symbol[arrayA$cpg %in% listA]
+        }
+      }
+      
       if(all(is.na(Background.list))){
         if(is.na(Background.Size)){
           stop("In case of use.annotation=F , one of the Background.list or Background.Size must be specified.")
@@ -40,7 +66,7 @@ CpG.Enrichment <- function(listA , listB ,Background.Size = NA, Background.list 
           N <- Background.Size
         }
       }else{
-        N <- length(Background.list)
+        N <- length(unique(Background.list))
       }
       
       n <- length(listA[!is.na(listA)])
@@ -54,7 +80,7 @@ CpG.Enrichment <- function(listA , listB ,Background.Size = NA, Background.list 
                           dimnames = list(B=c("inB","notB"),
                                           A=c("inA","notA")))
       f.test <- fisher.test(con.table, alternative = "greater")
-      results <- list(N.listB=m , N.Shared=k , P.value = f.test$p.value , Shared.CpGs = paste(shared.CpGs,collapse = ";"))
+      results <- list(N.listA = n, N.listB=m , N.Shared=k , P.value = f.test$p.value , Shared.ID = paste(shared.CpGs,collapse = ";"))
     }
   
   return(results)
