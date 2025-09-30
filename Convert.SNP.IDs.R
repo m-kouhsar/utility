a0441ce0fc19d8a04028d0d876fa78793d6ea553
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2025-09-30 11:15:18 +0100

    Update Convert.SNP.IDs.R

--- Convert.SNP.IDs.R
+++ Convert.SNP.IDs.R
@@ -1,7 +1,10 @@
+
+message("Loading reqiuered libraries...")
 library(biomaRt)
 library(data.table)
 arguments <- commandArgs(T)
 
+message("Reading input data...")
 Input.File <- arguments[1]
 Out.Prefix <- arguments[2]
 ID.col <- as.numeric(arguments[3])
@@ -11,14 +14,17 @@ Sourc.ID <- arguments[5]
 Genome.Ver <- match.arg(Genome.Ver , choices = c("GRCh37","GRCh38"))
 Sourc.ID <- match.arg(Sourc.ID , choices = c("rsid" , "chr:pos"))
 
+snp_ids = fread(file = Input.File , header = F , stringsAsFactors = F,data.table = F)
+
+message("Connecting to BioMart database...")
 if(Genome.Ver=="GRCh37"){
   snp_mart = useMart("ENSEMBL_MART_SNP", dataset="hsapiens_snp",host = "https://grch37.ensembl.org", verbose = T)
 }else if(Genome.Ver=="GRCh38"){
   snp_mart = useMart("ENSEMBL_MART_SNP", dataset="hsapiens_snp", verbose = T)
 }
 snp_attributes = c("refsnp_id", "chr_name", "chrom_start","chrom_end","allele","allele_1")
-snp_ids = fread(file = Input.File , header = F , stringsAsFactors = F,data.table = F)
 
+message("Converting IDs...")
 if(Sourc.ID == "rsid" ){
   
   snp_filters = c("snp_filter")
@@ -31,10 +37,5 @@ if(Sourc.ID == "rsid" ){
   
 }
 
+message("Writing the results...")
 fwrite(results, file = paste0(Out.Prefix , "SNP.ID.txt"), row.names = F , sep = "\t",quote = F)
-
-
-
-
-
-
