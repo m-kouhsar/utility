0c355a298b896064146bfce45fb4d46cf40b9879
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2025-11-03 12:25:58 +0000

    Add files via upload

---
+++ Parallel.tar.gz.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -A Research_Project-MRC164847 # research project to submit under.
+#SBATCH --export=ALL # export all environment variables to the batch job.
+#SBATCH -D . # set working directory to .
+#SBATCH -p mrcq # submit to the parallel test queue
+#SBATCH --time=10:00:00 # Maximum wall time for the job
+#SBATCH --nodes=1 # specify number of nodes.
+#SBATCH --ntasks-per-node=16 # specify number of processors.
+#SBATCH --mail-type=END # send email at job completion
+#SBATCH --mail-user=m.kouhsar@exeter.ac.uk # email address
+#SBATCH --output=%x.%j.out
+#########################################################################
+
+SourceDir="$1"
+DestFilePrefix="$2"
+
+#########################################################################
+
+echo "Zipping $SourceDir ..."
+
+tar cf - $SourceDir | pigz > ${DestFilePrefix}.tar.gz
+
+echo "All done! The zipped file saved in $DestFilePrefix"
