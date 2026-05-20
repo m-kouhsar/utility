ee40aed652abf966dd1bd99aaadc3e0f8bbc0f29
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2026-05-20 11:41:19 +0100

    Update Convert.SNP.IDs.R
    Add description and update Input checking

--- Convert.SNP.IDs.R
+++ Convert.SNP.IDs.R
@@ -1,3 +1,15 @@
+########################################################################################################
+#                                                                                                      #
+# Using biomaRt and dbSNP to convert SNP IDs                                                           #
+#                                                                                                      #
+# Input arguments:                                                                                     #
+#                 Input.File: The file contains source IDs (rsid or chr:pos:Ref:Alt format)            #
+#                 Out.Prefix: Output files prefix (Directories will be created if it contains path)    #
+#                 ID.col: the source ID column name or index in the input file                         #
+#                 Genom.Ver: Genome version to use for conversion (GRCh37" or "GRCh38")                #
+#                 Source.ID: Source ID type ("rsid" or "chr:pos")                                      #
+#                                                                                                      #
+########################################################################################################
 
 message("Loading reqiuered libraries...")
 library(biomaRt)
@@ -5,16 +17,31 @@ library(data.table)
 arguments <- commandArgs(T)
 
 message("Reading input data...")
-Input.File <- arguments[1]
-Out.Prefix <- arguments[2]
-ID.col <- as.numeric(arguments[3])
-Genome.Ver <- arguments[4]
-Sourc.ID <- arguments[5]
+Input.File <- trimws(arguments[1])
+Out.Prefix <- trimws(arguments[2])
+ID.col <- trimws(arguments[3])
+Genome.Ver <- trimws(arguments[4])
+Sourc.ID <- trimws(arguments[5])
+
+Out.dir <- dirname(Out.Prefix)
+if(!dir.exists(Out.dir)){
+  dir.create(Out.dir , recursive = T)
+}
 
 Genome.Ver <- match.arg(Genome.Ver , choices = c("GRCh37","GRCh38"))
 Sourc.ID <- match.arg(Sourc.ID , choices = c("rsid" , "chr:pos"))
 
 snp_ids = fread(file = Input.File , header = F , stringsAsFactors = F,data.table = F)
+
+ID.col.num <- as.numeric(ID.col)
+
+if(is.na(ID.col.num)){
+  if(ID.col %in% names(snp_ids)){
+    ID.col.num <- match(ID.col,names(snp_ids))
+  }else{
+    stop("Can't find column name ",ID.col, " in the input file!")
+  }
+}
 
 message("Connecting to BioMart database...")
 if(Genome.Ver=="GRCh37"){
@@ -28,12 +55,12 @@ message("Converting IDs...")
 if(Sourc.ID == "rsid" ){
   
   snp_filters = c("snp_filter")
-  results = getBM(attributes=snp_attributes, filters=snp_filters, values=snp_ids[,ID.col], mart=snp_mart)
+  results = getBM(attributes=snp_attributes, filters=snp_filters, values=snp_ids[,ID.col.num], mart=snp_mart)
   
 } else if(Sourc.ID == "chr:pos"){
   
   snp_filters = c("chr_name","start","end")
-  results = getBM(attributes=snp_attributes, filters=snp_filters, values=snp_ids[,ID.col], mart=snp_mart)
+  results = getBM(attributes=snp_attributes, filters=snp_filters, values=snp_ids[,ID.col.num], mart=snp_mart)
   
 }
 
